3abe48578ef63ced374569ddacc071aea06b8c21
zzeordyga <zzeordyga@gmail.com>
2022-05-20 16:53:43 +0700

    created nav and footer with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'ocean-green' : '#57CC99',
+        'metallic-blue' : '#22577A',
+        'keppel' : '#38A3A5',
+        'light-green' : '#80ED99',
+        'tea-green' : '#C7F9CC',
+      },
+    },
+  },
+  plugins: [],
+}
